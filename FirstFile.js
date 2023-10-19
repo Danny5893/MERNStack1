60a1287fd443b44e0cba6ed5a8996f44f828d2dd
danny <hvd5893@gmail.com>
2023-10-19 11:11:52 -0500

    first commit

---
+++ FirstFile.js
@@ -0,0 +1,11 @@
+console.log("This is the first execution from node js.")
+
+//to execute a js file we need reach to the path of file
+//cmd: node filename
+
+function add(a,b){
+    return a +b
+}
+
+console.log(add(7,8))
+
