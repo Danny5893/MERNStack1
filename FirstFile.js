35c741c8aa41461d4b7d1b9097c65dbf9030ed70
danny <hvd5893@gmail.com>
2023-10-23 11:08:16 -0500

    second commit

--- FirstFile.js
+++ FirstFile.js
@@ -3,9 +3,12 @@ console.log("This is the first execution from node js.")
 //to execute a js file we need reach to the path of file
 //cmd: node filename
 
+
 function add(a,b){
     return a +b
 }
 
 console.log(add(7,8))
 
+var myName = "BAC"
+console.log(myName)
---
+++ dataTypes.js
@@ -0,0 +1,38 @@
+var myValue = "This it my value"
+console.log(myValue)
+console.log(typeof myValue)
+
+myValue = 1000
+console.log(myValue)
+console.log(typeof myValue)
+
+myValue = 100.011
+console.log(myValue)
+console.log(typeof myValue)
+
+myValue = true
+console.log(myValue)
+console.log(typeof myValue)
+
+var object = {
+    name : "ABbc",
+    sex : "female"
+}
+console.log(object)
+
+myValue = object
+console.log(myValue)
+console.log(typeof myValue)
+
+myValue ={}
+console.log(myValue)
+console.log(typeof myValue)
+
+myValue = null
+console.log(myValue)
+console.log(typeof myValue)
+
+var mySymbol = Symbol("Basic symbol")
+console.log(mySymbol)
+console.log(typeof mySymbol)
+
